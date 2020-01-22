08203fa03a302089f376a11f298b36c77fe1fb87
Next EngGen3 <nextenggen3@Nexts-MacBook-Pro.local>
2020-01-22 12:34:15 +0200

    update register form

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { Form } from "./Form";
+import React, { useState } from "react";
 import "./form.css";
 import { useAction } from "../../redux/useAction";
 import { userActions } from "../../redux/user";
@@ -7,6 +6,10 @@ import { useHistory } from "react-router-dom";
 import { getLoggedUserData } from "../../data/dataManager";
 
 export function Login() {
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isValid = userName.length === 0 || password.length === 0;
   const loginUser = useAction(userActions.saveUser);
   const history = useHistory();
 
@@ -23,7 +26,36 @@ export function Login() {
   return (
     <div className="loginform">
       <h2>Login</h2>
-      <Form buttonTitle="Login" onSubmit={onSubmit}></Form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          setUsername("");
+          setPassword("");
+          onSubmit(userName, password);
+        }}
+      >
+        <div className="input">
+          User:
+          <input
+            required
+            onChange={e => setUsername(e.target.value)}
+            value={userName}
+          />
+        </div>
+        <div className="input">
+          Password:
+          <input
+            type="password"
+            required
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <button className="button" disabled={isValid}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
+
